dcf84382d1cd2425a05452c212ee45b8885b6441
Shaktiraj Daudra <shaktirajdaudra@gmail.com>
2020-08-31 19:09:27 +0530

    added more pages

--- js/lets-make-it-better.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.parallax').parallax();
-});
-$(document).ready(function(){
-    $('.scrollspy').modal();
-});
-$(document).ready(function(){
-    $('#modal1').modal();
-});
