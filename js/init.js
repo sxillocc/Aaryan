af0d470ff1610ea70846de841a23ba9faceb5823
Mohit Arora <arora.mohit1122@gmail.com>
2020-09-11 11:30:02 +0530

    article.html

--- js/init.js
+++ js/init.js
@@ -7,3 +7,10 @@
 })(jQuery); // end of jQuery name space
 
 $(".dropdown-trigger").dropdown();
+
+$('.post-wrapper').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
