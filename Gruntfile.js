9904a3fe3e57b4458291e89d3083fb3af28c13b4
Shaktiraj Daudra <shaktirajdaudra@gmail.com>
2020-12-02 16:54:51 +0530

    changes-2

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,10 +9,22 @@ module.exports = function(grunt) {
           rename: false,
           sizes: [{
             name: '',
+            width: '400px',
+            suffix: '_w400',
+            quality: 50
+          },
+          {
+            name: '',
             width: '650px',
             suffix: '_w650',
             quality: 50
           },
+          {
+            name: '',
+            width: '840px',
+            suffix: '_w840',
+            quality: 50
+          }
         ]
         },
         files: [{
