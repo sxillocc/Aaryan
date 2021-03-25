b05a8d0269a11e89db1124db384a54826e1698d3
Shaktiraj Daudra <shaktirajdaudra@gmail.com>
2021-03-25 14:29:33 +0530

    x

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,28 +10,28 @@ module.exports = function(grunt) {
           sizes: [{
             name: '',
             width: '400px',
-            suffix: '_w400',
+            suffix: '_50w400',
             quality: 50
           },
           {
             name: '',
             width: '650px',
-            suffix: '_w650',
+            suffix: '_50w650',
             quality: 50
           },
           {
             name: '',
             width: '840px',
-            suffix: '_w840',
+            suffix: '_50w840',
             quality: 50
           }
         ]
         },
         files: [{
           expand: true,
-          src: ['*.{png,jpg}'],
-          cwd: './original-images',
-          dest: './buffer'
+          src: ['ram.jpg'],
+          cwd: './buffer',
+          dest: './'
         }]
       }
     },
